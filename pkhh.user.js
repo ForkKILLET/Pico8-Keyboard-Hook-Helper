b620a6e23d4526987ff48fd1453ca83ecf568a4f
ForkΨKILLET <fork_killet@qq.com>
2022-04-19 18:29:59 +0800

    release: 0.2
    feat(hook): Add `wrap` mode.
    feat(ui): Add hook mode options and a debug button.

--- pkhh.user.js
+++ pkhh.user.js
@@ -1,16 +1,19 @@
 // ==UserScript==
 // @name         Pico8 Keyboard Hook Helper
 // @namespace    https://icelava.top/
-// @version      0.1
-// @description  Enable user to customize key bindings.
+// @version      0.2
+// @description  Enable the player to customize key bindings.
 // @author       ForkKILLET <fork_killet@qq.com>
 // @match        https://v6p9d9t4.ssl.hwcdn.net/html/5587462/index.html
+// @match        https://v6p9d9t4.ssl.hwcdn.net/html/3226817/index.html
+// @match        https://v6p9d9t4.ssl.hwcdn.net/html/235259/Celeste/index.html?v=1542780913
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        unsafeWindow
+// @run-at       document-start
 // ==/UserScript==
 
-const log = (s, ...p) => console.log(`[pkhh] ${s}`, ...p)
+const log = (s, ...p) => console.log(`%c[pkhh]%c ${s}`, "display: inline-block; background-color: blue; color: white; border-radius: 3px; padding: 1px 3px;", "", ...p)
 
 const sto = new Proxy({}, {
     get: (_, k) => GM_getValue(k),
@@ -67,7 +70,13 @@ const drawUserInterface = () => {
     $ui.id = "pkhh-ui"
     $ui.innerHTML = `
         <style>${css}</style>
-        <b>Pico8 Keyboard Hook Helper</p>
+        <b>Pico8 Keyboard Hook Helper</b> <i>v${GM_info.script.version}</i><br />
+        <a href="https://github.com/ForkKILLET/Pico8-Keyboard-Hook-Helper">GitHub</a> <button id="pkhh-expose">debug</button>
+        <hr />
+        Hook mode <select id="pkhh-hook-mode">
+            <option>wrap</option>
+            <option>re-trigger</option>
+        </select>
         <hr />
         Key bindings <button id="pkhh-new">+</button>
         <ul id="pkhh-kmap"></ul>
@@ -106,35 +115,68 @@ const drawUserInterface = () => {
         })
     }
 
+    const $hook_mode = document.getElementById("pkhh-hook-mode")
+    $hook_mode.value = sto.hook_mode ??= "wrap"
+    $hook_mode.addEventListener("change", () => {
+        sto.hook_mode = $hook_mode.value
+    })
+
+    const $expose = document.getElementById("pkhh-expose")
+    $expose.addEventListener("click", () => {
+        log("Expose to `window.pkhh`")
+        unsafeWindow.pkhh = { sto, GM_info }
+    })
+
     const $kmap = document.getElementById("pkhh-kmap")
     for (const [ from, [ to, to_code ] ] of Object.entries(sto.kmap ??= {})) drawItem(from, [ to, to_code ])
 
     const $new = document.getElementById("pkhh-new")
     $new.addEventListener("click", () => drawItem("", ""))
-
-    log("drawUserInterface OK")
 }
 
-const hookKeyboardEvent = (name, doLog) => {
-	document.addEventListener(name, e => {
+const hookKeyboardEvent = n => {
+	document.addEventListener(n, e => {
         if (hotkeyGlobalState === 1) return
-		if (e.new) return
+		if (e.new || e.TAS) return
 		e.stopPropagation()
 		const kmap = sto.kmap
-		const ne = new KeyboardEvent(name, {
-			key: kmap[e.key] ?? e.key,
-			keyCode: kmap[e.key]?.[1] ?? e.key
+		const ne = new KeyboardEvent(n, {
+			key: kmap[e.key]?.[0] ?? e.key,
+			keyCode: kmap[e.key]?.[1] ?? e.keyCode
 		})
 		ne.new = true
-		if (doLog) {
-			console.log("e: %o", e)
-			console.log("ne: %o", ne)
-		}
 		document.dispatchEvent(ne)
 	})
 }
 
+const hookKeyboardEventListener = wrapper => {
+    const _addEventListener = Element.prototype.addEventListener
+    window.addEventListener = document.addEventListener = function (name, listener) {
+        _addEventListener.call(this, name, name === "keydown" || name === "keyup"
+            ? wrapper(name, listener)
+            : listener
+        )
+    }
+}
 
-hookKeyboardEvent("keydown")
-hookKeyboardEvent("keyup")
 drawUserInterface()
+log("Draw UI")
+
+switch (sto.hook_mode ??= "wrap") {
+    case "re-trigger":
+        hookKeyboardEvent("keydown")
+        hookKeyboardEvent("keyup")
+        break
+    case "wrap":
+        hookKeyboardEventListener((n, f) => e => {
+            const kmap = sto.kmap
+            const ne = new KeyboardEvent(n, {
+                key: kmap[e.key]?.[0] ?? e.key,
+                keyCode: kmap[e.key]?.[1] ?? e.keyCode
+            })
+            f(ne)
+        })
+        break
+}
+
+log("Hook")
